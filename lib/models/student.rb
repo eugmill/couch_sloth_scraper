57131df6a0c82ef8270788c8c914b9cfddfe8681
Michael Prouty <michael.prouty@gmail.com>
2014-02-14 15:52:21 -0500

    Finish student model with name, quote, image_url, and biography

--- lib/models/student.rb
+++ lib/models/student.rb
@@ -1,31 +1,37 @@
 class Student
-  attr_accessor :name, :tagline, :biography, :education_list, :work_list 
+  attr_accessor :name, :quote, :biography, :image_url
   
 
-  def initialize
-
+  def initialize(student_hash) 
+    self.name = student_hash[:name]
+    self.quote = student_hash[:quote]
+    self.biography = student_hash[:biography]
+    self.image_url = student_hash[:image_url]
   end
 
   def self.create_table
     sql_statement = <<-SQL
       CREATE TABLE IF NOT EXISTS students 
       (id INTEGER PRIMARY KEY AUTOINCREMENT, 
-      name TEXT, tagline TEXT)
+      name TEXT, quote TEXT, biography TEXT, image_url TEXT)
     SQL
     DB.execute(sql_statement)
-    DB.execute("SELECT tagline FROM students")
   end
   
   def save
-    DB.execute("SELECT tagline FROM students")
-    DB.execute("INSERT INTO students (name, tagline) VALUES ((:name),(:tagline))", to_h)
-    # DB.execute("INSERT INTO movies (name) VALUES (?) WHERE name = ?", name)
+    sql_statement = <<-SQL
+      INSERT INTO students (name, quote, biography, image_url) 
+      VALUES ((:name), (:quote), (:biography), (:image_url))
+    SQL
+    DB.execute(sql_statement, to_h)
   end
 
   def to_h
     {
       :name => name,
-      :tagline => tagline
+      :quote => quote, 
+      :biography => biography,
+      :image_url => image_url
     }
   end
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,59 +2,66 @@ require_relative 'spec_helper'
 
 describe "Student" do
   before(:each) do
-    DB.execute("CREATE TABLE students (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, tagline TEXT)")
+    DB.execute("CREATE TABLE students (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, quote TEXT, biography TEXT, image_url TEXT)")
+    @test_hash = {
+      :name => "Eugene",
+      :quote => "I'm a cool dude ;P",
+      :biography => "Hangin round, downtown by myself and I've had too much caffeine.",
+      :image_url => "http://test.com/image.png"
+    }
   end
+
   after(:each) do
     DB.execute("DROP TABLE students")
   end
 
+
   describe "-model" do 
-    # Model itself
-    it 'has a name' do
-      m = Student.new
-      m.name = "Bob Saget"
-
-      expect(m.name).to eq("Bob Saget")
-    end
-
-    it 'has a tagline' do
-      m = Student.new
-      m.tagline = "Hello there"
-
-      expect(m.tagline).to eq("Hello there")
+    it 'has attributes passed in on initialization' do
+      s = Student.new(@test_hash)
+      expect(s.name).to eq("Eugene")
+      expect(s.quote).to eq("I'm a cool dude ;P")
+      expect(s.biography).to eq("Hangin round, downtown by myself and I've had too much caffeine.")
+      expect(s.image_url).to eq("http://test.com/image.png")
     end
   end
+
+
 
   # Hash tests
   describe "#to_h" do  
     it 'converts attributes to a hash' do
-      m = Student.new
-      m.name = "Bob Saget"
-      m.tagline = "Hello there"
-
-      expect(m.to_h).to eq(
-        {
-          :name => "Bob Saget",
-          :tagline => "Hello there"
-        }
-      )
+      s = Student.new(@test_hash)
+      expect(s.to_h).to eq(
+     {
+      :name => "Eugene",
+      :quote => "I'm a cool dude ;P",
+      :biography => "Hangin round, downtown by myself and I've had too much caffeine.",
+      :image_url => "http://test.com/image.png"
+    }
+    )
     end
   end
-  describe "-database" do
-    it 'should save itself or persist to a database' do
-      m = Student.new
-      m.name = "Bob Saget"
-      m.tagline = "Hello there"
 
-      m.save
+  describe "-database" do
+    it "Should save and retrieve it's attributes" do
+      s = Student.new(@test_hash)
+      s.save
 
       # Go into the database and see if there is a row with 
       # the movie's data
 
-      results = DB.execute("SELECT name,tagline FROM students WHERE name = 'Bob Saget'")
-      
-      expect(results[0][0]).to eq("Bob Saget")
-      expect(results[0][1]).to eq("Hello there")    
+      results = DB.execute("SELECT name FROM students")
+      expect(results[0][0]).to eq("Eugene")
+
+      results = DB.execute("SELECT quote FROM students")
+      expect(results[0][0]).to eq("I'm a cool dude ;P")    
+
+      results = DB.execute("SELECT biography FROM students")
+      expect(results[0][0]).to eq("Hangin round, downtown by myself and I've had too much caffeine.")    
+
+      results = DB.execute("SELECT image_url FROM students")
+      expect(results[0][0]).to eq("http://test.com/image.png")    
     end
   end
 end
