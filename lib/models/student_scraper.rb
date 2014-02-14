648adafd593f9b768d000a21e567a8ab719399c5
chriskohlbrenner <c.kohlbrenner@gmail.com>
2014-02-14 15:52:47 -0500

    more scrape info methods

--- lib/models/student_scraper.rb
+++ lib/models/student_scraper.rb
@@ -7,33 +7,37 @@ class StudentScraper
   end
 
   def scrape
-      scrape_name
-      # scrape_information
+      scrape_one_line_information
+      binding.pry
       create_instance
     rescue
       NoMethodError # no method for 404s
       binding.pry
   end
 
+    def scrape_one_line_information
+    scrape_name
+    scrape_quote
+    scrape_image_url
+    scrape_biography
+    # scrape_education
+    # scrape_work
+  end
+
   def scrape_name
       @name = @doc.search("div.page-title h4.ib_main_header").text
   end
 
+  def scrape_quote
+      @quote = @doc.search("div.textwidget h3").text
+  end
 
-  # def scrape_information
-  #     name = @doc.search("div.page-title h4.ib_main_header").text
-  #     image_url = @doc.search("div.page-title h4.ib_main_header").text)
-  #     tagline = @doc.search("div.page-title h4.ib_main_header").text
-  #     biography = @doc.search("div.page-title h4.ib_main_header").text)
-  #     education = @doc.search("div.page-title h4.ib_main_header").text
-  #     work = @doc.search("div.page-title h4.ib_main_header").text
-  #   rescue
-  #     NoMethodError # no method for 404s
-  #   @information = {
+  def scrape_image_url
+      @image_url = @doc.search("div.top-page-title div img").attribute("src").value
+  end
 
-
-
-  #   }
+  # def scrape_biography
+  #     @biography = @doc.search("div#scroll-about div#ok-text-column-2[style=height: 204px;].column div.services p").text
   # end
 
   def create_instance
