6259c779e7ca36696742e03fb1a208c2f2d32d78
Eugene Millerman <e.v.millerman@gmail.com>
2014-02-14 14:52:58 -0500

    Add tagline

--- lib/models/student.rb
+++ lib/models/student.rb
@@ -1,16 +1,31 @@
 class Student
-  attr_accessor :name
+  attr_accessor :name, :tagline, :biography, :education_list, :work_list 
+  
+
+  def initialize
+
+  end
 
   def self.create_table
-    DB.execute("CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)")
+    sql_statement = <<-SQL
+      CREATE TABLE IF NOT EXISTS students 
+      (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+      name TEXT, tagline TEXT)
+    SQL
+    DB.execute(sql_statement)
+    DB.execute("SELECT tagline FROM students")
   end
   
   def save
-    DB.execute("INSERT INTO students (name) VALUES (:name)", to_h)
+    DB.execute("SELECT tagline FROM students")
+    DB.execute("INSERT INTO students (name, tagline) VALUES ((:name),(:tagline))", to_h)
     # DB.execute("INSERT INTO movies (name) VALUES (?) WHERE name = ?", name)
   end
 
   def to_h
-    {:name => name}
+    {
+      :name => name,
+      :tagline => tagline
+    }
   end
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,37 +2,59 @@ require_relative 'spec_helper'
 
 describe "Student" do
   before(:each) do
-    DB.execute("CREATE TABLE students (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)")
+    DB.execute("CREATE TABLE students (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, tagline TEXT)")
   end
   after(:each) do
     DB.execute("DROP TABLE students")
   end
 
-  it 'has a name' do
-    m = Student.new
-    m.name = "Bob Saget"
+  describe "-model" do 
+    # Model itself
+    it 'has a name' do
+      m = Student.new
+      m.name = "Bob Saget"
 
-    expect(m.name).to eq("Bob Saget")
+      expect(m.name).to eq("Bob Saget")
+    end
+
+    it 'has a tagline' do
+      m = Student.new
+      m.tagline = "Hello there"
+
+      expect(m.tagline).to eq("Hello there")
+    end
   end
 
-  it 'converts attributes to a hash' do
-    m = Student.new
-    m.name = "Bob Saget"
+  # Hash tests
+  describe "#to_h" do  
+    it 'converts attributes to a hash' do
+      m = Student.new
+      m.name = "Bob Saget"
+      m.tagline = "Hello there"
 
-    expect(m.to_h).to eq({:name => "Bob Saget"})
+      expect(m.to_h).to eq(
+        {
+          :name => "Bob Saget",
+          :tagline => "Hello there"
+        }
+      )
+    end
   end
-  
-  it 'should save itself or persist to a database' do
-    m = Student.new
-    m.name = "Bob Saget"
+  describe "-database" do
+    it 'should save itself or persist to a database' do
+      m = Student.new
+      m.name = "Bob Saget"
+      m.tagline = "Hello there"
 
-    m.save
+      m.save
 
-    # Go into the database and see if there is a row with 
-    # the movie's data
+      # Go into the database and see if there is a row with 
+      # the movie's data
 
-    results = DB.execute("SELECT name FROM students WHERE name = 'Bob Saget'")
-    
-    expect(results[0][0]).to eq("Bob Saget")    
+      results = DB.execute("SELECT name,tagline FROM students WHERE name = 'Bob Saget'")
+      
+      expect(results[0][0]).to eq("Bob Saget")
+      expect(results[0][1]).to eq("Hello there")    
+    end
   end
 end
