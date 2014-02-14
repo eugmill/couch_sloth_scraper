d1a87a9ce99cb3527b2f93cab47675eea678679d
chriskohlbrenner <c.kohlbrenner@gmail.com>
2014-02-14 14:55:44 -0500

    changed runner and student_scraper.rb from movies to students

--- lib/models/student_scraper.rb
+++ lib/models/student_scraper.rb
@@ -1,21 +1,33 @@
-class MovieScraper
+class StudentScraper
   def initialize(url)
-    @doc = Nokogiri::HTML(open(url))
+      @doc = Nokogiri::HTML(open(url))
+    rescue
+      OpenURI::HTTPError # Erik Peterson and Corbin Page profile 404
   end
 
   def scrape
     scrape_name
+    scrape_information
     create_instance
   end
 
   def scrape_name
-    @name = @doc.search("h1.header span.itemprop[itemprop=name]").text
+      @name = @doc.search("div.page-title h4.ib_main_header").text
+    rescue
+      NoMethodError # no method for 404s
+  end
+
+  def scrape_information
+    @information = {
+
+
+    }
   end
 
   def create_instance
-    @movie = Movie.new
-    @movie.name = @name
-    @movie.save
-    @movie
+    @student = Student.new
+    @student.name = @name
+    # @student.save
+    @student
   end
 end
